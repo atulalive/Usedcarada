407aa55cd180e1276cc65f7bdefe81d664986424
atulalive <94101683+atulalive@users.noreply.github.com>
2021-12-06 18:51:22 +0530

    top city page

---
+++ app/Views/usedcar_topcities_view.php
@@ -0,0 +1,131 @@
+
+<div class="container ">
+        <div class="card">
+            <div class="card-body">
+   
+        <div class="row">
+            <div class="col-md-6" style="font-size:10px">
+                <div class=" center-block text-left">
+                    <h2>Used Cars In Top Cities</h2>
+                    <hr class="deep-purple  text-primary accent-2 mb-4 mt-0 d-inline-block mx-auto" style="padding-top:20px">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="text-right">
+
+                    <h2 style="font-size:18px">I am looking to buy a second hand car in</h2><br><br>
+
+                    <a href="#location" data-toggle="modal" class="text-dark"> </a>
+
+                    <select id="select_page" style="width:300px;height:50px;" class="operator">
+                        <option value=""><br>Select city...</option>
+                        <option value="Pune">Pune</option>
+                        <option value="Mumbai">Mumbai</option>
+                        <option value="India">India</option>
+                        <option value="England">England</option>
+                    </select>
+                    <br><br><br>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-2 col-lg-4 col-md-6 ">
+                    <a href="cars-list.html" class="top-cities card text-center mb-xl-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/001-statue-of-liberty.svg" alt="img" class="bg-pink-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Ahmedabad</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6">
+                    <a href="cars-list.html" class="top-cities card text-center mb-xl-0 mb-lg-5 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/017-taj-mahal.svg" alt="img" class="svg2 bg-purple-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Bangalore</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6">
+                    <a href="cars-list.html" class="top-cities card text-center mb-xl-0 mb-lg-5 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/031-stonehenge.svg" alt="img" class="bg-warning-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Chennai</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6">
+                    <a href="cars-list.html" class="top-cities card text-center mb-lg-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/002-sydney-opera-house.svg" alt="img" class="bg-danger-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Delhi NCR</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6">
+                    <a href="cars-list.html" class="top-cities card text-center mb-sm-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/003-brandenburg-gate.svg" alt="img" class="bg-success-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Gurgaon</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6">
+                    <a href="cars-list.html" class="top-cities card text-center mb-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/010-great-wall-of-china.svg" alt="img" class="bg-info-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Hyderabad</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6 mt-2" >
+                    <a href="cars-list.html" class="top-cities card text-center mb-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/001-statue-of-liberty.svg" alt="img" class="bg-info-transparent">
+                        <div class="servic-data mt-3"class="d-inline">
+                            <h4 class="font-weight-semibold mb-0">Jaipur</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6 mt-2">
+                    <a href="cars-list.html" class="top-cities card text-center mb-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/017-taj-mahal.svg" alt="img" class="bg-info-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Kollkata</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6 mt-2">
+                    <a href="cars-list.html" class="top-cities card text-center mb-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/002-sydney-opera-house.svg" alt="img" class="bg-info-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Mumbai</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6 mt-2">
+                    <a href="cars-list.html" class="top-cities card text-center mb-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/031-stonehenge.svg" alt="img" class="bg-info-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">New Delhi</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6 mt-2">
+                    <a href="cars-list.html" class="top-cities card text-center mb-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/003-brandenburg-gate.svg" alt="img" class="bg-info-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Noida</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6 mt-2">
+                    <a href="cars-list.html" class="top-cities card text-center mb-0 box-shadow2">
+                        <img src="<?= URL_IMAGES?>svgs/cities/010-great-wall-of-china.svg" alt="img" class="bg-info-transparent">
+                        <div class="servic-data mt-3">
+                            <h4 class="font-weight-semibold mb-0">Pune</h4>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div></div></div>
+</div>
+</section>
